e329e82dde0425d41b595c528627b1346bb7f5d7
Natasha <randomkeyz@gmail.com>
2018-06-03 23:45:58 -0400

    Fixing caching and offline functionality

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
+var cache_name = 'restaurant-v6';
 self.addEventListener('install', function(event){	
 	event.waitUntil(
-		caches.open('restaurant-v5').then(function(cache){
+		caches.open(cache_name).then(function(cache){
 			return cache.addAll([
 				'/',
 				'restaurant.html',
@@ -39,7 +40,19 @@ self.addEventListener('fetch', function(event){
 	event.respondWith(
 		caches.match(event.request, {'ignoreSearch': true}).then(function(response){
 			if(response) return response;
-			return fetch(event.request);
+			
+			var fetchRequest = event.request.clone();
+			return fetch(fetchRequest).then(function (response) {
+
+				if(!response) return response;
+
+				var responseToCache = response.clone();
+				caches.open(cache_name).then(function(cache){
+					cache.put(event.request, responseToCache)
+				});
+
+				return response;
+			});
 		}).catch(function(error){
 			console.log(event.request);
 			console.log('Error: ' + error)
